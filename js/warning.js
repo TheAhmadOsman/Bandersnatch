ba3425fd2da4535a13bb52a1a9178c45c4ec5bff
Ahmad M. Osman <ahmad.osman@luther.edu>
2018-12-31 13:25:37 -0600

    info

--- js/warning.js
+++ js/warning.js
@@ -3,7 +3,8 @@ Swal({
     confirmButtonClass: "btn btn-secondary btn-sm",
     buttonsStyling: false,
     title: "Warning!",
-    html: "Spoilers ahead! If you haven't watched the Movie already, you should head to Netflix!<br><br>" +
+    html: "Spoilers ahead! If you haven't watched the Movie already, you should head to Netflix!" +
+        '<br> <br> FYI, you can scroll up, down, right, and left the graph! Enjoy!<br><br>' +
         'This website is developed by <a href="https://twitter.com/XMasterrrr">@XMasterrrr</a>',
     confirmButtonColor: 'grey',
     confirmButtonText: "Got it!"
