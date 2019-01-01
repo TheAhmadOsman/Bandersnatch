ccbdf3eb7354492324b8f2e96fcbb0f0dcb3202c
Ahmad M. Osman <ahmad.osman@luther.edu>
2019-01-01 04:52:49 -0600

    strong

--- js/warning.js
+++ js/warning.js
@@ -3,10 +3,10 @@ Swal({
     confirmButtonClass: "btn btn-secondary btn-sm",
     buttonsStyling: false,
     title: "Warning!",
-    html: "<b>Spoilers ahead!</b> If you haven't watched the Movie already, you should head to Netflix!" +
-        '<br> <br> FYI, you can scroll <b>up, down, right, and left</b> the paths! Enjoy!<br><br>' +
-        '<b>Developed by <a href="https://twitter.com/XMasterrrr" target="_blank">@XMasterrrr</a>  - ' + 
-        'I am looking for a summer internship, feel free to <a href="https://ahmadosman.com/#contact" target="_blank">contact me</a>!</b>',
+    html: "<strong>Spoilers ahead!</strong> If you haven't watched the Movie already, you should head to Netflix!" +
+        '<br> <br> FYI, you can scroll <strong>up, down, right, and left</strong> the paths! Enjoy!<br><br>' +
+        '<strong>Developed by <a href="https://twitter.com/XMasterrrr" target="_blank">@XMasterrrr</a>  - ' + 
+        'I am looking for a summer internship, feel free to <a href="https://ahmadosman.com/#contact" target="_blank">contact me</a>!</strong>',
     confirmButtonColor: 'grey',
     confirmButtonText: "Got it!"
 });
