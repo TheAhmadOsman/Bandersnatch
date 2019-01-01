b8b2fe518e24fff3f509931a561c357633ba28c5
Ahmad M. Osman <ahmad.osman@luther.edu>
2019-01-01 04:40:11 -0600

    blank target

--- js/warning.js
+++ js/warning.js
@@ -5,7 +5,7 @@ Swal({
     title: "Warning!",
     html: "Spoilers ahead! If you haven't watched the Movie already, you should head to Netflix!" +
         '<br> <br> FYI, you can scroll up, down, right, and left the paths! Enjoy!<br><br>' +
-        'Developed by <a href="https://twitter.com/XMasterrrr">@XMasterrrr</a>  - ' + 
+        'Developed by <a href="https://twitter.com/XMasterrrr" target="_blank">@XMasterrrr</a>  - ' + 
         'I am looking for a summer internship, feel free to <a href="https://ahmadosman.com/#contact" target="_blank">contact me</a>',
     confirmButtonColor: 'grey',
     confirmButtonText: "Got it!"
