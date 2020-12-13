f0693a7f4ad8da0c5fbe3c8c1880539710954fc0
Ahmad M. Osman <me@ahmadosman.com>
2020-12-13 17:10:00 -0600

    Changing the warning message

--- js/warning.js
+++ js/warning.js
@@ -6,7 +6,7 @@ Swal({
     html: "<strong>Spoilers ahead!</strong> If you haven't watched the Movie already, you should head to Netflix!" +
         '<br><br>FYI, you can scroll <b>&#8593;</b> up <b>&#8595;</b> down <b>&#8592;</b> left and <b>&#8594;</b> right the paths! Enjoy!<br><br>' +
         '<strong>Developed by <a href="https://twitter.com/XMasterrrr" target="_blank">@XMasterrrr</a>  - ' + 
-        'I am looking for a summer internship, feel free to <a href="https://ahmadosman.com/#contact" target="_blank">contact me</a>!</strong>',
+        'Feel free to <a href="https://ahmadosman.com/#contact" target="_blank">contact me on my website too</a>!</strong>',
     confirmButtonColor: 'grey',
     confirmButtonText: "Got it!"
 });
